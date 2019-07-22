3ea378046c2d502cab4cb533ce62c63f26e95447
Hiroyuki Adachi <hiroxpepe@gmail.com>
2019-07-22 22:43:13 +0900

    added the dev-environment of webpack and SCSS.

--- website-webapp/src/main/webapp/index.jsp
+++ website-webapp/src/main/webapp/index.jsp
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
     <head>
         <title>Start Page</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script language="javascript" type="text/javascript" src="<c:url value="/docroot/scripts/webapp.min.lib.js"/>"></script>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1 class="hoge">Hello World!</h1>
     </body>
 </html>
---
+++ website-webapp/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+    mode: "development", // to replace "production" in start the service.
+    entry: [
+        './src/main/resources/assets/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'src/main/webapp/docroot/scripts'),
+        filename: 'webapp.min.lib.js'
+    },
+    devtool: 'source-map', // to delete in start the service.
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ]
+    }
+};
